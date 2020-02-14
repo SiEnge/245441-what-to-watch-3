d705466faa172139b141ff168cea0c93e0a33dbc
sienge <enge666-86@yandex.ru>
2020-02-14 21:46:14 +0300

    Добавлена деструктуризация movie в movie-small-card

--- src/components/small-movie-card/small-movie-card.jsx
+++ src/components/small-movie-card/small-movie-card.jsx
@@ -2,23 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const SmallMovieCard = (props) => {
-  const {movie, onTitleClick, onCardHover} = props;
+  const {movie: {id, title, poster}, onTitleClick, onCardHover} = props;
 
   return (
     <article
-      onMouseOver={() => onCardHover(movie.id)}
+      onMouseOver={() => onCardHover(id)}
       onMouseOut={() => onCardHover(-1)}
       className="small-movie-card catalog__movies-card"
     >
       <div className="small-movie-card__image">
-        <img src={movie.poster} alt={movie.title} width="280" height="175" />
+        <img src={poster} alt={title} width="280" height="175" />
       </div>
       <h3 className="small-movie-card__title">
         <a
           onClick={onTitleClick}
           className="small-movie-card__link"
           href="movie-page.html"
-        >{movie.title}</a>
+        >{title}</a>
       </h3>
     </article>
   );
