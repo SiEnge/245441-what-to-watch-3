d3adea2a16009f493951c21e5d659b4688bf7a79
sienge <enge666-86@yandex.ru>
2020-02-27 23:54:59 +0300

    Исправлен e2e-тест для SmallMovieCard

--- src/components/small-movie-card/small-movie-card.test.js
+++ src/components/small-movie-card/small-movie-card.test.js
@@ -2,12 +2,15 @@ import React from "react";
 import renderer from "react-test-renderer";
 import SmallMovieCard from "../small-movie-card/small-movie-card.jsx";
 import {movie} from "../../utils/test.utils.js";
+const isPlaying = false;
+
 
 it(`Render SmallMovieCard`, () => {
   const tree = renderer
     .create(<SmallMovieCard
       key={movie.id}
       movie={movie}
+      isPlaying={isPlaying}
       onMovieCardClick={() => {}}
       onCardHover={() => {}}
     />)
---
+++ src/components/video-player/video-player.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import VideoPlayer from "../video-player/video-player.jsx";
+import {movie} from "../../utils/test.utils.js";
+
+const {poster, preview} = movie;
+
+it(`Render VideoPlayer`, () => {
+  const tree = renderer
+    .create(<VideoPlayer
+      poster={poster}
+      preview={preview}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
