bec035e822f1d72a7bddaecd040493ab452f2c5c
sienge <enge666-86@yandex.ru>
2020-03-23 10:50:42 +0300

    К index.js подключены thunk, applyMiddleware и createAPI

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
+import thunk from "redux-thunk";
 import App from "./components/app/app.jsx";
 import {reducer} from "./reducer.js";
+import {createAPI} from "./api.js";
 
 const promoMovie = {
   title: `The Grand Budapest Hotel`,
