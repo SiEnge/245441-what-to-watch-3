76ba81d9bbd95ea50b96cdf282345dcb73c758b7
sienge <enge666-86@yandex.ru>
2020-03-25 14:59:11 +0300

    Добавлено в user: ActionCreator, ActionType, Operator

--- src/reducer/user/user.js
+++ src/reducer/user/user.js
@@ -1,3 +1,5 @@
+import {extend} from "../../utils/common.js";
+
 const AuthorizationStatus = {
   AUTH: `AUTH`,
   NO_AUTH: `NO_AUTH`
@@ -7,10 +9,35 @@ const initialState = {
   authorizationStatus: AuthorizationStatus.NO_AUTH,
 };
 
+const ActionType = {
+  UPDATE_AUTH_STATUS: `UPDATE_AUTH_STATUS`,
+};
+
+const ActionCreator = {
+  updateAuthStatus: (authStatus) => ({
+    type: ActionType.UPDATE_AUTH_STATUS,
+    payload: authStatus,
+  }),
+};
+
+const Operation = {
+  updateAuthStatus: () => (dispatch, getState, api) => {
+    return api.get(`/login`)
+      .then((response) => {
+        dispatch(ActionCreator.updateAuthStatus(response.data));
+      });
+  },
+};
+
 
 const reducer = (state = initialState, action) => {
-
+  switch (action.type) {
+    case ActionType.UPDATE_AUTH_STATUS:
+      return extend(state, {
+        authorizationStatus: action.payload,
+      });
+  }
   return state;
 };
 
-export {reducer};
+export {reducer, Operation, ActionType, ActionCreator};
