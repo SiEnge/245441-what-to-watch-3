61bef4e1b56528b9d426f8326c4866355acad603
sienge <enge666-86@yandex.ru>
2020-03-25 12:51:31 +0300

    Создан компонент SignIn

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SignIn = (props) => {
+const SignIn = () => {
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
@@ -20,11 +20,11 @@ const SignIn = (props) => {
           <div className="sign-in__fields">
             <div className="sign-in__field">
               <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" />
-              <label className="sign-in__label visually-hidden" for="user-email">Email address</label>
+              <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
             <div className="sign-in__field">
               <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
-              <label className="sign-in__label visually-hidden" for="user-password">Password</label>
+              <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
           </div>
           <div className="sign-in__submit">
